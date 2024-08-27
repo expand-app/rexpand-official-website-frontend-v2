4bf0939e2ab45d7e42e7c3342da951e8ee73c74e
weiqi yang <weiqi.yang@rexpandco.com>
2024-08-27 17:15:32 +0800

    feat alumni page

---
+++ src/pages/ad/californiaIrvine/data.ts
@@ -0,0 +1,86 @@
+import GoldmanSachs_m from "@/assets/ad/godman.svg";
+import Amazon_m from "@/assets/ad/amazon.svg";
+import Google_m from "@/assets/ad/google.svg";
+import Meta_m from "@/assets/ad/meta.svg";
+import blackRock_m from "@/assets/ad/blackRock.svg";
+import payPal_m from "@/assets/ad/paypal.svg";
+
+import goldmanSachs from "@/assets/ad/GoldmanSachs.png";
+import Amazon from "@/assets/ad/amazon.png";
+import GoldmanSachs from "@/assets/ad/GoldmanSachs.png";
+import Google from "@/assets/ad/google.png";
+import Meta from "@/assets/ad/meta.png";
+import blackRock from "@/assets/ad/blackRock.png";
+import payPal from "@/assets/ad/payPal.png";
+
+export const REFERRALS_DATA = [
+  {
+    id: 1,
+    name: "Chris(Xuemei) Zhang",
+    avatar:
+      "https://resources.rexpandcareer.com/image/California%2C%20Irvine/Chris%28Xuemei%29%20Zhang.jpeg",
+    headLine: "CEO at Ivy surrogacy",
+    url: "https://www.linkedin.com/in/chrisxuemeizhang",
+    bioIntro:
+      "Chris Zhang currently serves as Chief Executive Officer at Ivy Surrogacy. Throughout her career, Chris has held key roles at renowned companies, including Program Director and Hanabusa IVF. She earned her Master of Business Administration from the University of California, Irvine, in 2016. Chris resides and works in Los Angeles.",
+  },
+  {
+    id: 2,
+    name: "David Chen",
+    avatar:
+      "https://resources.rexpandcareer.com/image/California%2C%20Irvine/David%20Chen.jpeg",
+    headLine: "Head of Finance Systems at Pinterest | ex Amazon, Uber",
+    url: "https://www.linkedin.com/in/davidtchen/",
+    bioIntro:
+      "David Chen currently serves as Head of Finance Systems at Pinterest. Throughout his career, David has made significant contributions to renowned companies such as Amazon and Facebook. He earned his MBA in Information Technology from the University of California, Irvine, in 2005. David resides and works in the Greater Seattle Area.",
+  },
+  {
+    id: 3,
+    name: "Harvey Ma",
+    avatar:
+      "https://resources.rexpandcareer.com/image/California%2C%20Irvine/Harvey%20Ma.jpeg",
+    headLine: "Founder & Managing Partner at Aedge Ventures",
+    url: "https://www.linkedin.com/in/harveyma/",
+    bioIntro:
+      "Harvey Ma currently serves as Founder & Managing Partner at Aedge Ventures. Throughout his career, Harvey has made significant contributions to renowned companies such as Ackuretta and AREA 02. He earned his BA in Honors Economics from the University of California, Irvine, in 2000. Harvey resides and works in Taipei.",
+  },
+  {
+    id: 4,
+    name: "Wayne Liu",
+    avatar:
+      "https://resources.rexpandcareer.com/image/California%2C%20Irvine/Wayne%20Liu.jpeg",
+    headLine: "Managing Director at Deloitte Consulting",
+    url: "https://www.linkedin.com/in/wayne-liu-819560/",
+    bioIntro:
+      "Wayne Liu currently serves as Managing Director at Deloitte Consulting. Throughout his career, Wayne has made significant contributions to renowned companies such as Commerce Bank and United Heritage Bank. He earned his MBA in Finance from the University of California, Irvine, in 2009. Wayne resides and works in Los Angeles.",
+  },
+  {
+    id: 5,
+    name: "Wilson Wang",
+    avatar:
+      "https://resources.rexpandcareer.com/image/California%2C%20Irvine/Wilson%20Wang.jpeg",
+    headLine: "Director Of Sales Operations at Microsoft",
+    url: "https://www.linkedin.com/in/wilsontwang/",
+    bioIntro:
+      "Wilson Wang is currently the Director of Operations at Microsoft. Throughout his career, Wilson has made significant contributions to renowned companies such as Fleet Laboratories and Kingston. He earned his BA in Psychology from the University of California, Irvine, in 2007. Wilson resides and works in Washington, D.C.",
+  },
+];
+
+export const COMPANY_LIST = [
+  GoldmanSachs,
+  Amazon,
+  Google,
+  Meta,
+  blackRock,
+  payPal,
+];
+export const COMPANY_LIST_MIN = [
+  GoldmanSachs_m,
+  Amazon_m,
+  Google_m,
+  Meta_m,
+  blackRock_m,
+  payPal_m,
+];
+
+export const AD_LINK = "https://work.weixin.qq.com/ca/cawcded5d8e6da62cb";
