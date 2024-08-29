2742521984ba019edc0ea27f51204bce899a3901
weiqi yang <weiqi.yang@rexpandco.com>
2024-08-29 18:46:34 +0800

    feat ad alumni page

---
+++ src/pages/ad/Illinois/data.ts
@@ -0,0 +1,86 @@
+import GoldmanSachs_m from "@/assets/ad/godman.svg";
+import Amazon_m from "@/assets/ad/amazon.svg";
+import Google_m from "@/assets/ad/google.svg";
+import Meta_m from "@/assets/ad/meta.svg";
+import blackRock_m from "@/assets/ad/blackRock.svg";
+import payPal_m from "@/assets/ad/paypal.svg";
+
+import goldmanSachs from "@/assets/ad/GoldmanSachs.png";
+import Amazon from "@/assets/ad/amazon.png";
+import GoldmanSachs from "@/assets/ad/GoldmanSachs.png";
+import Google from "@/assets/ad/google.png";
+import Meta from "@/assets/ad/meta.png";
+import blackRock from "@/assets/ad/blackRock.png";
+import payPal from "@/assets/ad/payPal.png";
+
+export const REFERRALS_DATA = [
+  {
+    id: 1,
+    name: "Claire Deng",
+    avatar:
+      "https://resources.rexpandcareer.com/image/Illinois%20at%20Urbana-Champaign/Claire%20Deng.jpeg",
+    headLine: "Managing Director at BlackRock",
+    url: "https://www.linkedin.com/in/claire-deng-5a87bb7",
+    bioIntro:
+      "Claire Deng currently serves as Managing Director at BlackRock. Throughout her career, Claire has made significant contributions to renowned companies. She earned her PhD in Finance from the University of Illinois at Urbana-Champaign in 2007. Claire resides and works in New York.",
+  },
+  {
+    id: 2,
+    name: "Ding Zhang",
+    avatar:
+      "https://resources.rexpandcareer.com/image/Illinois%20at%20Urbana-Champaign/Ding%20Zhang.jpeg",
+    headLine: "Head of Engineering at Stacked",
+    url: "https://www.linkedin.com/in/ding-zhang",
+    bioIntro:
+      "Ding Zhang currently serves as Head of Engineering, Americas at Stacked. Throughout his career, Ding has made significant contributions to renowned companies such as UberEats and Verizon Media. He earned his Bachelor’s Degree in Computer Science from the University of Illinois at Urbana-Champaign in 2015. Ding resides and works in California.",
+  },
+  {
+    id: 3,
+    name: "CMO at Nogin",
+    avatar:
+      "https://resources.rexpandcareer.com/image/Illinois%20at%20Urbana-Champaign/CMO at Nogin.jpeg",
+    headLine: "CMO at Nogin",
+    url: "https://www.linkedin.com/in/munan",
+    bioIntro:
+      "Munan currently serves as CMO at Nogin. Throughout his career, Munan has made significant contributions to renowned companies such as NOBULL and Rugs USA. He earned his Bachelor’s Degree in Electrical Engineering from the University of Illinois at Urbana-Champaign in 2008. Munan resides and works in New York.",
+  },
+  {
+    id: 4,
+    name: "Vernon Hui",
+    avatar:
+      "https://resources.rexpandcareer.com/image/Illinois%20at%20Urbana-Champaign/Vernon%20Hui.jpeg",
+    headLine: "CTO at OpenGov",
+    url: "https://www.linkedin.com/in/vernonh",
+    bioIntro:
+      "Vernon currently serves as Chief Technology Officer at OpenGov Inc. Throughout his career, Vernon has made significant contributions to renowned companies such as Qualtrics and Oracle. He earned his Bachelor of Science in Computer Engineering from the University of Illinois at Urbana-Champaign in 1998. Vernon resides and works in Washington.",
+  },
+  {
+    id: 5,
+    name: "Xin Li",
+    avatar:
+      "https://resources.rexpandcareer.com/image/Illinois%20at%20Urbana-Champaign/Xin%20Li.jpeg",
+    headLine: "CTO of JD Health, Head of Product and Tech",
+    url: "https://www.linkedin.com/in/lixinlx",
+    bioIntro:
+      "Xin Li currently serves as CTO (Head of Product & Tech) at JD Health International Inc. Throughout his career, Lixin has made significant contributions to renowned companies such as JD.COM and Lazada. He earned his PhD in Computer Science from the University of Illinois at Urbana-Champaign in 2005. Lixin resides and works in Greater Seattle.",
+  },
+];
+
+export const COMPANY_LIST = [
+  GoldmanSachs,
+  Amazon,
+  Google,
+  Meta,
+  blackRock,
+  payPal,
+];
+export const COMPANY_LIST_MIN = [
+  GoldmanSachs_m,
+  Amazon_m,
+  Google_m,
+  Meta_m,
+  blackRock_m,
+  payPal_m,
+];
+
+export const AD_LINK = "https://work.weixin.qq.com/ca/cawcde633c6a485e5e";
---
+++ src/pages/ad/californiaSanDiego/data.ts
@@ -0,0 +1,87 @@
+import GoldmanSachs_m from "@/assets/ad/godman.svg";
+import Amazon_m from "@/assets/ad/amazon.svg";
+import Google_m from "@/assets/ad/google.svg";
+import Meta_m from "@/assets/ad/meta.svg";
+import blackRock_m from "@/assets/ad/blackRock.svg";
+import payPal_m from "@/assets/ad/paypal.svg";
+
+import goldmanSachs from "@/assets/ad/GoldmanSachs.png";
+import Amazon from "@/assets/ad/amazon.png";
+import GoldmanSachs from "@/assets/ad/GoldmanSachs.png";
+import Google from "@/assets/ad/google.png";
+import Meta from "@/assets/ad/meta.png";
+import blackRock from "@/assets/ad/blackRock.png";
+import payPal from "@/assets/ad/payPal.png";
+
+export const REFERRALS_DATA = [
+  {
+    id: 1,
+    name: "Gary Wang",
+    avatar:
+      "https://resources.rexpandcareer.com/image/%20California%2C%20San%20Diego/Gary%20Wang.jpeg",
+    headLine: "CEO and co-founder at HOME",
+    url: "https://www.linkedin.com/in/eddiewen/",
+    bioIntro:
+      "Gary Wang currently serves as CEO and co-founder at HOME. Throughout his career, Gary has made significant contributions to renowned companies such as RE/MAX Elite Realty and The Hertz Corporation. He earned his Bachelor of Arts in Economics from the University of California, San Diego, in 2005. Gary resides and works in California.",
+  },
+  {
+    id: 2,
+    name: "Hong Hou",
+    avatar:
+      "https://resources.rexpandcareer.com/image/%20California%2C%20San%20Diego/Hong%20Hou.jpeg",
+    headLine: "President, CEO & Board Member at Semtech",
+    url: "https://www.linkedin.com/in/hong-hou-2a5541",
+    bioIntro:
+      "Hong Hou currently serves as President, CEO, and Board Member at Semtech. Throughout his career, Hong has made significant contributions to renowned companies such as Brooks Automation and Intel Corporation. He earned his PhD in Electrical Engineering from the University of California, San Diego, in 1993. Hong resides and works in California.",
+  },
+  {
+    id: 3,
+    name: "Jenny Zhang",
+    avatar:
+      "https://resources.rexpandcareer.com/image/%20California%2C%20San%20Diego/Jenny%20Zhang.jpeg",
+    headLine:
+      "Managing Director, Head of Portfolio, Pricing & Analytics at Goldman Sachs",
+    url: "https://www.linkedin.com/in/jennyzhang212",
+    bioIntro:
+      "Jenny Zhang currently serves as Managing Director at Goldman Sachs. Throughout her career, Jenny has made significant contributions to renowned companies such as AIG and StepStone Group. She earned her Bachelor’s Degree in Mathematics and Economics from the University of California, San Diego, in 2011. Jenny resides and works in New York.",
+  },
+  {
+    id: 4,
+    name: "Xidi Wang",
+    avatar:
+      "https://resources.rexpandcareer.com/image/%20California%2C%20San%20Diego/Xidi%20Wang.jpeg",
+    headLine: "Founder at Quantum Strategics",
+    url: "https://www.linkedin.com/in/xidi-wang-8231ab11",
+    bioIntro:
+      "Xidi Wang is currently the Founder of Quantum Strategics. Throughout his career, Xidi has made significant contributions to renowned companies such as Citibank and Credicard. He earned his PhD in Physics from the University of California, San Diego, in 1990. Xidi resides and works in California.",
+  },
+  {
+    id: 5,
+    name: "Yicong Ma",
+    avatar:
+      "https://resources.rexpandcareer.com/image/%20California%2C%20San%20Diego/Yicong%20Ma.jpeg",
+    headLine: "Founder and CEO at Renaissance Intelligence",
+    url: "https://www.linkedin.com/in/jeffwang11",
+    bioIntro:
+      "Yicong Ma currently serves as the Founder and CEO of Renaissance Intelligence. Throughout her career, Yicong has made significant contributions to renowned companies such as Smule, Inc. and IBM. She earned her PhD in Physics from the University of California, San Diego, in 2015. Yicong resides and works in San Francisco.",
+  },
+];
+
+export const COMPANY_LIST = [
+  GoldmanSachs,
+  Amazon,
+  Google,
+  Meta,
+  blackRock,
+  payPal,
+];
+export const COMPANY_LIST_MIN = [
+  GoldmanSachs_m,
+  Amazon_m,
+  Google_m,
+  Meta_m,
+  blackRock_m,
+  payPal_m,
+];
+
+export const AD_LINK = "https://work.weixin.qq.com/ca/cawcde633c6a485e5e";
