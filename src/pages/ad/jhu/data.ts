fcbe16728b571a53b9422e07f012105db5fa2a09
weiqi yang <weiqi.yang@rexpandco.com>
2024-09-05 17:49:36 +0800

    feat code

---
+++ src/pages/ad/jhu/data.ts
@@ -0,0 +1,86 @@
+import GoldmanSachs_m from "@/assets/ad/godman.svg";
+import Amazon_m from "@/assets/ad/amazon.svg";
+import Google_m from "@/assets/ad/google.svg";
+import Meta_m from "@/assets/ad/meta.svg";
+import blackRock_m from "@/assets/ad/blackRock.svg";
+import payPal_m from "@/assets/ad/paypal.svg";
+
+import goldmanSachs from "@/assets/ad/GoldmanSachs.png";
+import Amazon from "@/assets/ad/amazon.png";
+import GoldmanSachs from "@/assets/ad/GoldmanSachs.png";
+import Google from "@/assets/ad/google.png";
+import Meta from "@/assets/ad/meta.png";
+import blackRock from "@/assets/ad/blackRock.png";
+import payPal from "@/assets/ad/payPal.png";
+
+export const REFERRALS_DATA = [
+  {
+    id: 1,
+    name: "Andrew Chang",
+    avatar:
+      "https://resources.rexpandcareer.com/image%2FJHU%2FAndrew%20Chang.jpeg",
+    headLine: "Growth at Bill.com | Fintech & SaaS | Growth & Product Leader",
+    url: "https://www.linkedin.com/in/andrewgchang/",
+    bioIntro:
+      "Andrew Chang is currently the Head of Customer Marketing at BILL. Throughout his career, Andrew has held key roles at renowned companies such as Invoice2go and The Coca-Cola Company. He earned a Bachelor of Arts in International Studies, Economics, and Entrepreneurship & Management from Johns Hopkins University in 2007. Andrew currently resides and works in San Francisco.",
+  },
+  {
+    id: 2,
+    name: "David Song",
+    avatar:
+      "https://resources.rexpandcareer.com/image%2FJHU%2FDavid%20Song.jpeg",
+    headLine: "CTO / CSM / Executive Architect at IBM",
+    url: "https://www.linkedin.com/in/david-song-14089010/",
+    bioIntro:
+      "David Song currently serves as CTO, CSM, and Executive Architect at IBM. Throughout his career, David has contributed to several renowned companies, including Noblis and Tetra Tech. He earned a Master’s degree in Information & Telecommunication Systems from Johns Hopkins University in 2008. David currently resides and works in Washington.",
+  },
+  {
+    id: 3,
+    name: "Eric Chen",
+    avatar:
+      "https://resources.rexpandcareer.com/image%2FJHU%2FEric%20Chen.jpeg",
+    headLine: "CTO at Alucio",
+    url: "https://www.linkedin.com/in/emily-wang/",
+    bioIntro:
+      "Eric Chen is currently the Chief Technology Officer at Alucio. Throughout his career, Eric has held significant positions at renowned organizations, including the American Society of Clinical Oncology (ASCO) and Oration. He earned a degree in Biomedical Engineering from Johns Hopkins University. Eric currently resides and works in California.",
+  },
+  {
+    id: 4,
+    name: "Vivian Tang",
+    avatar:
+      "https://resources.rexpandcareer.com/image%2FJHU%2FVivian%20Tang.jpeg",
+    headLine: "Principal at Boston Consulting Group (BCG)",
+    url: "https://www.linkedin.com/in/frank-lin/",
+    bioIntro:
+      "Vivian Chen is currently a Principal at Boston Consulting Group (BCG). Throughout her career, Vivian has held key positions at several renowned companies, including CapTech Ventures, Inc. and Booz Allen Hamilton. She earned a Master’s degree in Biotechnology with a concentration in Biodefense from Johns Hopkins University in 2014. Vivian currently resides and works in Washington.",
+  },
+  {
+    id: 5,
+    name: "Michael Y.",
+    avatar:
+      "https://resources.rexpandcareer.com/image%2FJHU%2FMichael%20Y..jpeg",
+    headLine: "Head of Operations & Technology, Founding Team at Boram",
+    url: "https://www.linkedin.com/in/grace-chen/",
+    bioIntro:
+      "Mo Yu is currently the Head of Operations & Technology and part of the founding team at Boram. Throughout his career, Mo has held key roles at renowned companies such as Palate and Claro Partners. He earned a Bachelor of Science in Computer Engineering from Johns Hopkins University in 2006. Mo currently resides and works in New York.",
+  },
+];
+
+export const COMPANY_LIST = [
+  GoldmanSachs,
+  Amazon,
+  Google,
+  Meta,
+  blackRock,
+  payPal,
+];
+export const COMPANY_LIST_MIN = [
+  GoldmanSachs_m,
+  Amazon_m,
+  Google_m,
+  Meta_m,
+  blackRock_m,
+  payPal_m,
+];
+
+export const AD_LINK = "https://work.weixin.qq.com/ca/cawcde2cacaf3de744";
---
+++ src/pages/ad/neu/data.ts
@@ -0,0 +1,85 @@
+import GoldmanSachs_m from "@/assets/ad/godman.svg";
+import Amazon_m from "@/assets/ad/amazon.svg";
+import Google_m from "@/assets/ad/google.svg";
+import Meta_m from "@/assets/ad/meta.svg";
+import blackRock_m from "@/assets/ad/blackRock.svg";
+import payPal_m from "@/assets/ad/paypal.svg";
+
+import goldmanSachs from "@/assets/ad/GoldmanSachs.png";
+import Amazon from "@/assets/ad/amazon.png";
+import GoldmanSachs from "@/assets/ad/GoldmanSachs.png";
+import Google from "@/assets/ad/google.png";
+import Meta from "@/assets/ad/meta.png";
+import blackRock from "@/assets/ad/blackRock.png";
+import payPal from "@/assets/ad/payPal.png";
+
+export const REFERRALS_DATA = [
+  {
+    id: 1,
+    name: "Lebin Cheng",
+    avatar:
+      "https://resources.rexpandcareer.com/image%2FNEU%2FLebin%20Cheng.jpeg",
+    headLine: "Head of API Security at Imperva",
+    url: "https://www.linkedin.com/in/lebin/",
+    bioIntro:
+      "Lebin Cheng is currently the Head of API Security at Imperva. Throughout his career, Lebin has held significant roles at several renowned companies, including CloudVector and ArecaBay. He earned a Bachelor of Science in Computer Science from Northeastern University in 1994. Lebin currently resides and works in San Francisco.",
+  },
+  {
+    id: 2,
+    name: "Li Yan",
+    avatar: "https://resources.rexpandcareer.com/image%2FNEU%2FLi%20Yan.jpeg",
+    headLine: "Principal at BCG | Procurement | Cost Optimization | Digital",
+    url: "https://www.linkedin.com/in/li-yan-bcg/",
+    bioIntro:
+      "Yan Li is currently a Principal at Boston Consulting Group (BCG). Throughout her career, Yan has held significant roles at renowned companies such as Accenture and Tyco International. She earned a Bachelor of Science in Business Administration with concentrations in Supply Chain Management, Finance, and Psychology from Northeastern University in 2012. Yan currently resides and works in Greater Houston.",
+  },
+  {
+    id: 3,
+    name: "Ming Fang",
+    avatar:
+      "https://resources.rexpandcareer.com/image%2FNEU%2FMing%20Fang.jpeg",
+    headLine: "Partner and COO at American Vision Capital",
+    url: "https://www.linkedin.com/in/ming-fang-b2b7633/",
+    bioIntro:
+      "Ming Fang is currently the Co-Founder and COO of American Vision Capital. Throughout his career, Ming has held key positions at several renowned companies, including Bluebeam, Inc. and IRIS.TV. He earned a Bachelor of Engineering in Computer Engineering from Northeastern University in 1999. Ming currently resides and works in Los Angeles.",
+  },
+  {
+    id: 4,
+    name: "Shawn Wen",
+    avatar:
+      "https://resources.rexpandcareer.com/image%2FNEU%2FShawn%20Wen.jpeg",
+    headLine: "Chief Technology Officer at LSQ",
+    url: "https://www.linkedin.com/in/nancy-lee/",
+    bioIntro:
+      "Shawn Wen is currently the Chief Technology Officer at LSQ. Throughout his career, Shawn has held key roles at renowned companies such as Spark Insights and Ottometric. He earned a Master’s degree in Information Systems from Northeastern University in 1999. Shawn currently resides and works in Greater Boston.",
+  },
+  {
+    id: 5,
+    name: "Xiaolin Hou",
+    avatar:
+      "https://resources.rexpandcareer.com/image%2FNEU%2FXiaolin%20Hou.jpeg",
+    headLine: "CTO at Vividing Inc.",
+    url: "https://www.linkedin.com/in/oscar-wang/",
+    bioIntro:
+      "Xiaolin Lin is currently the CTO at Vividing Inc. Throughout his career, Xiaolin has held key positions at several renowned companies, including Fidelity Investments. He earned a Master’s degree in Math from Northeastern University in 1991. Xiaolin currently resides and works in Massachusetts.",
+  },
+];
+
+export const COMPANY_LIST = [
+  GoldmanSachs,
+  Amazon,
+  Google,
+  Meta,
+  blackRock,
+  payPal,
+];
+export const COMPANY_LIST_MIN = [
+  GoldmanSachs_m,
+  Amazon_m,
+  Google_m,
+  Meta_m,
+  blackRock_m,
+  payPal_m,
+];
+
+export const AD_LINK = "https://work.weixin.qq.com/ca/cawcdefb024d826f20";
